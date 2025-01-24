71ccfccf8e78698aab426853b57d69b627a6074e
LAPTOP-OR4J58MA\TANG YUKUN <e1297789@u.nus.edu>
2025-01-24 12:31:42 +0800

    Modify Alpha.java and Task.java

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -19,10 +19,52 @@ public class Alpha {
             System.out.println(" The list is empty.");
         } else {
             for (int i = 0; i < list.size(); i++) {
-                System.out.println(" " + (i + 1) + ". " + list.get(i)); // Display index and task
+                System.out.println(" " + (i + 1) + "." + list.get(i)); // Display index and task
             }
         }
         System.out.println("____________________________________________________________");
+    }
+
+    private static void markTask(int index) {
+        if (index >= 0 && index < list.size()) {
+            Task task = list.get(index);
+            if (task.isMarked()) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" This task has been marked.");
+                System.out.println("____________________________________________________________");
+            } else {
+                task.mark();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + task);
+                System.out.println("____________________________________________________________");
+            }
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid index: " + (index + 1));
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    private static void unmarkTask(int index) {
+        if (index >= 0 && index < list.size()) {
+            Task task = list.get(index);
+            if (!task.isMarked()) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" This task has been unmarked.");
+                System.out.println("____________________________________________________________");
+            } else {
+                task.unMark();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + task);
+                System.out.println("____________________________________________________________");
+            }
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid index: " + (index + 1));
+            System.out.println("____________________________________________________________");
+        }
     }
 
     private static void greeting() {
@@ -50,13 +92,33 @@ public class Alpha {
         String input = in.nextLine();
         String[] words = input.split("\\s+");
         while (!input.equals("bye")) {
-            if (words[0].equals("list")) {
-                showList();
-            } else if (words[0].equals("blah")) {
-                blah();
-            } else {
-                String description = String.join(" ", words);
-                add(new Task(description));
+            switch (words[0]) {
+                case "list" -> showList();
+                case "blah" -> blah();
+                case "mark" -> {
+                    try {
+                        int index = Integer.parseInt(words[1]) - 1; // Convert to 0-based index
+                        markTask(index);
+                    } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Invalid format. Use: mark <index>");
+                        System.out.println("____________________________________________________________");
+                    }
+                }
+                case "unmark" -> {
+                    try {
+                        int index = Integer.parseInt(words[1]) - 1; // Convert to 0-based index
+                        unmarkTask(index);
+                    } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Invalid format. Use: unmark <index>");
+                        System.out.println("____________________________________________________________");
+                    }
+                }
+                default -> {
+                    String description = String.join(" ", words);
+                    add(new Task(description));
+                }
             }
             input = in.nextLine();
             words = input.split("\\s+");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,30 @@
 public class Task {
     private final String taskName;
+    private boolean done;
 
     Task(String taskName) {
         this.taskName = taskName;
+        done = false;
+    }
+
+    protected boolean isMarked() {
+        return done;
+    }
+
+    protected void mark() {
+        done = true;
+    }
+
+    protected void unMark() {
+        done = false;
     }
 
     @Override
     public String toString() {
-        return taskName;
+        if (done) {
+            return "[X] " + taskName;
+        } else {
+            return "[ ] " + taskName;
+        }
     }
 }
